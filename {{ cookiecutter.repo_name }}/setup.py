93b7bc9fca6cc1b11470e73245ac579b1c7abb53
Sebastian Schneider <sschneider@point-8.de>
2018-07-23 16:22:06 +0200

    Add stuff for module initializing.

---
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
+
+setup(
+    name='{{ cookiecutter.module_name }}',
+    description="{{ cookiecutter.description }}",
+    author="{{ cookiecutter.author_name.replace('\"', '\\\"') }}",
+    author_email='{{ cookiecutter.email }}',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3 :: Only',
+{%- if cookiecutter.open_source_license in license_classifiers %}
+        '{{ license_classifiers[cookiecutter.open_source_license] }}',
+{%- endif %}
+    ],
+{%- if cookiecutter.open_source_license in license_classifiers %}
+    license="{{ cookiecutter.open_source_license }}",
+{%- endif %}
+    # long_description=readme + '\n\n' + history,a
+    packages=find_packages(include=['{{ cookiecutter.module_name }}']),
+    # setup_requires=setup_requirements,
+    # test_suite='tests',
+    # tests_require=test_requirements,
+)
---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for {{ cookiecutter.repo_name }}."""
+
+__author__ = """{{ cookiecutter.author_name }}"""
